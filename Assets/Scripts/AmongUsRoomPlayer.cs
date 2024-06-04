1ca946ba91450870d829dad01788af641314b5cd
민성 <noblesi1570@gmail.com>
2024-06-04 16:41:12 +0900

    Update LobbyPlayer 세팅

--- Assets/Scripts/AmongUsRoomPlayer.cs
+++ Assets/Scripts/AmongUsRoomPlayer.cs
@@ -2,8 +2,44 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Mirror;
+using UnityEngine.Rendering;
 
 public class AmongUsRoomPlayer : NetworkRoomPlayer
 {
-    
+    [SyncVar]
+    public EPlayerColor playerColor;
+
+    private void SpawnLobbyPlayerCharacter()
+    {
+        var roomSlots = (NetworkManager.singleton as AmongUsRoomManager).roomSlots;
+        EPlayerColor color = EPlayerColor.Red;
+
+        for(int i = 0; i < (int)EPlayerColor.Lime + 1; i++)
+        {
+            bool isFindSameColor = false;
+            foreach(var roomPlayer in roomSlots)
+            {
+                var amongUsRoomPlayer = roomPlayer as AmongUsRoomPlayer;
+                if(amongUsRoomPlayer.playerColor == (EPlayerColor)i && roomPlayer.netId != netId)
+                {
+                    isFindSameColor = true;
+                    break;
+                }
+            }
+
+            if(!isFindSameColor)
+            {
+                color = (EPlayerColor)i;
+                break;
+            }
+            
+        }
+        playerColor = color;
+
+
+        //Vector3 spawnPos = FindObjectOfType<SpawnPositions>().GetSpawnPosition();
+
+        //var player = Instantiate(AmongUsRoomManager.singleton.spawnPrefabs[0], spawnPos, Quaternion.identity);
+        //NetworkServer.Spawn(player, connectionToClient);
+    }
 }
