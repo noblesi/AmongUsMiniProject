f6cf89e57720bd1ec8e9c10c8a68cdf609709142
민성 <noblesi1570@gmail.com>
2024-06-07 00:22:20 +0900

    Update 조종실, 산소실, 무기실 배치 완료

---
+++ Assets/Scripts/WeaponPipeLight.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponPipeLight : MonoBehaviour
+{
+    private Animator animator;
+
+    private WaitForSeconds wait = new WaitForSeconds(0.15f);
+
+    private List<WeaponPipeLight> lights = new List<WeaponPipeLight>();
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+        for(int i = 0; i < transform.childCount; i++)
+        {
+            var child = transform.GetChild(i).GetComponent<WeaponPipeLight>();
+            if (child)
+            {
+                lights.Add(child);
+            }
+        }
+    }
+
+    public void TurnOnLight()
+    {
+        animator.SetTrigger("On");
+        StartCoroutine(TurnOnLightAtChild());
+    }
+
+    private IEnumerator TurnOnLightAtChild()
+    {
+        yield return wait;
+
+        foreach(var child in lights)
+        {
+            child.TurnOnLight();
+        }
+    }
+}
